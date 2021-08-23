a315e1deff5dc5c2ef4dbde2c3498b3350f41738
Yash Garg <yashgarg5101@gmail.com>
2021-08-23 23:52:30 +0530

    JSX introduction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let name = "Yash";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav>
+        <ul>
+          <li className="nav-link">
+            <a href="/">Home</a>
+          </li>
+          <li className="nav-link">
+            <a href="/">About</a>
+          </li>
+          <li className="nav-link">
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <h1>Hello {name}</h1>
     </div>
   );
 }
